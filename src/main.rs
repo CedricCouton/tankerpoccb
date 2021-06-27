048816f591b833db48a704a2c05a7a9f580b61d9
Cédric Couton <cedric.couton@doctolib.com>
2021-06-27 22:19:50 +0200

    try fix

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use tankersdk::{Options, Core};
 
-fn main() {
-    let _ = couchbase_lite::Database::open(&std::path::Path::new("."), couchbase_lite::DatabaseConfig::default());
+#[link(name = "tanker", modifiers = "+whole-archive")]
+extern "C" {
+    pub fn tanker_status(tanker: *mut u8) -> u8;
+}
 
-    if let Ok(mut runtime) = tokio::runtime::Runtime::new() {
+fn main() {
+    println!("test1");
+   if let Ok(mut runtime) = tokio::runtime::Runtime::new() {
         runtime.block_on(async move {
             let tanker = Core::new(Options::new("test".to_string(), "/tmp".to_owned())).await.unwrap();
             println!("{:?}", tanker);
         });
     }
+    println!("test2");
+
+    let _ = couchbase_lite::Database::open(&std::path::Path::new("."), couchbase_lite::DatabaseConfig::default());
+
+    println!("test3");
+
+    unsafe { tanker_status(std::ptr::null_mut()); }
+
 }
