cb9818d5e35dfacc114bdb2867cf7d6a82811a37
jerem <jeremy.tellaa@tanker.io>
2021-07-05 16:15:47 +0200

    strip

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
 use tankersdk::{Options, Core};
 
 fn main() {
-    //let _ = couchbase_lite::Database::open(&std::path::Path::new("."), couchbase_lite::DatabaseConfig::default());
-
-    if let Ok(mut runtime) = tokio::runtime::Runtime::new() {
-        runtime.block_on(async move {
-            let tanker = Core::new(Options::new("test".to_string(), "/tmp".to_owned())).await.unwrap();
-            println!("{:?}", tanker);
-        });
-    }
+    let _tanker = Core::new(Options::new("test".to_string(), "/tmp".to_owned()));
 }
